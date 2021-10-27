7d0775a28014a712b07efad449b95e7b423e97ac
Pedro Vieira <pedrovieira@arrowplan.com>
2021-10-27 17:45:07 +0200

    Create test file

---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 27 17:39 2021
+
+@author: Pedro Vieira
+@description: Implement the test functions for the VSCNN network
+"""
