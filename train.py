a28a151b5c3a269318779bd5347f19162c1079ce
Pedro Vieira <pedrovieira@arrowplan.com>
2021-10-27 17:40:43 +0200

    Move code from CrossTrain to train.py for it to be a base for the reimplementation

--- train.py
+++ train.py
@@ -1,8 +1,8 @@
 import torch
 from torch import nn, optim
 import numpy as np
-from scipy.io import loadmat
-from utils import weight_init, loadLabel
+from scipy.io import loadmat, savemat
+from utils import weight_init, loadLabel, preprocess, selectValuableSample
 from HSIDataset import HSIDataset, DatasetInfo
 from Model.module import VSCNN_KSC, VSCNN_PaviaU
 from torch.utils.data import DataLoader
@@ -65,71 +65,94 @@ def test(model, criterion, dataLoader):
     return acc, np.mean(evalLoss)
 
 
-# def main(datasetName, n_sample_per_class, run, encoderPath=None):
+# isExists = lambda path: os.path.exists(path)
+# SAMPLE_PER_CLASS = [10, 50, 100]
+# RUN = 10
+# EPOCHS = 10
+# LR = 1e-1
+# BATCHSZ = 10
+# NUM_WORKERS = 8
+# SEED = 971104
+# torch.manual_seed(SEED)
+# DEVICE = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+# viz = Visdom()
+# ROOT = None
+# N_SELECT = 4
+#
+# def main(datasetName, n_sample_per_class, run):
 #     # 加载数据和标签
 #     info = DatasetInfo.info[datasetName]
 #     data_path = "./data/{}/{}.mat".format(datasetName, datasetName)
 #     label_path = './trainTestSplit/{}/sample{}_run{}.mat'.format(datasetName, n_sample_per_class, run)
 #     isExists(data_path)
 #     data = loadmat(data_path)[info['data_key']]
-#     bands = data.shape[2]
+#
 #     isExists(label_path)
 #     trainLabel, testLabel = loadLabel(label_path)
+#     res = torch.zeros((3, EPOCHS))
 #     # 数据转换
 #     data, trainLabel, testLabel = data.astype(np.float32), trainLabel.astype(np.int), testLabel.astype(np.int)
+#     # 数据预处理
+#     data = preprocess(data, info['n_component'])
+#     bands = data.shape[2]
+#     # 挑选有价值的样本
+#     s = int(np.sum(trainLabel != 0))
+#     iteration = int(np.math.ceil((0.8 * s - 90) / N_SELECT))
+#     trainLabel = selectValuableSample(data, trainLabel, iteration, N_SELECT)
+#
 #     nc = int(np.max(trainLabel))
-#     trainDataset = HSIDatasetV1(data, trainLabel, patchsz=42)
-#     testDataset = HSIDatasetV1(data, testLabel, patchsz=42)
+#     trainDataset = HSIDataset(data, trainLabel, patchsz=info['patchsz'])
+#     testDataset = HSIDataset(data, testLabel, patchsz=info['patchsz'])
 #     trainLoader = DataLoader(trainDataset, batch_size=BATCHSZ, shuffle=True, num_workers=NUM_WORKERS)
-#     testLoader = DataLoader(testDataset, batch_size=BATCHSZ, shuffle=True, num_workers=NUM_WORKERS)
-#     UNITS[0] = bands
-#     model = SSDL(UNITS, nc)
-#     model.apply(weight_init)
-#     # 加载编码器的预训练参数
-#     if encoderPath is not None:
-#         isExists(encoderPath)
-#         raw_dict = torch.load(encoderPath, map_location=torch.device('cpu'))
-#         target_dict = model.encoder.state_dict()
-#         keys = list(zip(raw_dict.keys(), target_dict.keys()))
-#         for raw_key, target_key in keys:
-#             target_dict[target_key] = raw_dict[raw_key]
-#         model.encoder.load_state_dict(target_dict)
+#     testLoader = DataLoader(testDataset, batch_size=128, shuffle=True, num_workers=NUM_WORKERS)
+#
+#     model = VSCNN_KSC(bands, nc) if datasetName == 'KSC' else VSCNN_PaviaU(bands, nc)
+#     # model.apply(weight_init)
 #     criterion = nn.CrossEntropyLoss()
-#     optimizer = optim.Adam(model.parameters(), lr=LR, weight_decay=1e-2)
-#     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.8)
+#     optimizer = optim.Adam(model.parameters(), lr=LR)
+#     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.8)
 #
 #     for epoch in range(EPOCHS):
 #         print('*'*5 + 'Epoch:{}'.format(epoch) + '*'*5)
 #         model, trainLoss = train(model, criterion=criterion, optimizer=optimizer, dataLoader=trainLoader)
 #         acc, evalLoss = test(model, criterion=criterion, dataLoader=testLoader)
-#         viz.line([[trainLoss, evalLoss]], [epoch], win='train&test loss', update='append')
-#         viz.line([acc], [epoch], win='accuracy', update='append')
 #         print('epoch:{} trainLoss:{:.8f} evalLoss:{:.8f} acc:{:.4f}'.format(epoch, trainLoss, evalLoss, acc))
 #         print('*'*18)
-#         # if os.path.isdir('./ssdl/{}/ssdl_sample{}_run{}_epoch{}'.format(datasetName, n_sample_per_class, run, epoch))
-#         if not os.path.isdir('./ssdl/{}'.format(datasetName)):
-#             os.makedirs('./ssdl/{}'.format(datasetName))
-#         torch.save(model.state_dict(), './ssdl/{}/ssdl_sample{}_run{}_epoch{}.pkl'.format(datasetName, n_sample_per_class, run, epoch))
+#         res[0][epoch], res[1][epoch], res[2][epoch] = trainLoss, evalLoss, acc
+#         if epoch%5==0:
+#             torch.save(model.state_dict(), os.path.join(ROOT, 'vscnn_sample{}_run{}_epoch{}.pkl'.format(n_sample_per_class,
+#                                                                                                        run, epoch)))
 #         scheduler.step()
-
-
+#     tmp = res.numpy()
+#     savemat(os.path.join(ROOT, 'res.mat'), {'trainLoss':tmp[0], 'evalLoss':tmp[1], 'acc':tmp[2]})
+#     return res
+#
+#
 # if __name__ == '__main__':
-#     parser = argparse.ArgumentParser(description='train ssdl')
-#     parser.add_argument('--name', type=str, default='PaviaU',
+#     parser = argparse.ArgumentParser(description='train VSCNN')
+#     parser.add_argument('--name', type=str, default='Salinas',
 #                         help='The name of dataset')
 #     parser.add_argument('--epoch', type=int, default=1,
 #                         help='模型的训练次数')
-#     parser.add_argument('--lr', type=float, default=1e-1,
+#     parser.add_argument('--lr', type=float, default=1e-3,
 #                         help='learning rate')
 #
 #     args = parser.parse_args()
 #     EPOCHS = args.epoch
 #     datasetName = args.name
 #     LR = args.lr
-#     # main(datasetName, n_sample_per_class, run, encoderPath=None)
-#     viz.line([[0., 0.]], [0.], win='train&test loss', opts=dict(title='train&test loss',
-#                                                                 legend=['train_loss', 'test_loss']))
-#     viz.line([0.,], [0.,], win='accuracy', opts=dict(title='accuracy',
-#                                                      legend=['accuracy']))
-#     main(datasetName, 10, 0, 'encoder/{}/{}_encoder_{}.ckpt'.format(datasetName, datasetName, 200))
+#     print('*'*5 + datasetName + '*'*5)
+#     for i, num in enumerate(SAMPLE_PER_CLASS):
+#         print('*' * 5 + 'SAMPLE_PER_CLASS:{}'.format(num) + '*' * 5)
+#         res = torch.zeros((RUN, 3, EPOCHS))
+#         for r in range(RUN):
+#             print('*' * 5 + 'run:{}'.format(r) + '*' * 5)
+#             ROOT = 'vscnn/{}/{}/{}'.format(datasetName, num, r)
+#             if not os.path.isdir(ROOT):
+#                 os.makedirs(ROOT)
+#             res[r] = main(datasetName, num, r)
+#         mean = torch.mean(res, dim=0)  # [3, EPOCHS]
+#         viz.line(mean.T, list(range(EPOCHS)), win='SAMPLE_PER_CLASS_{}'.format(num), opts=dict(title='SAMPLE_PER_CLASS_{}'.format(num),
+#                                                                                                legend=['train loss', 'test loss', 'acc']))
+#     print('*'*5 + 'FINISH' + '*'*5)
 
