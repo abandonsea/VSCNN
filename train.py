e0a2601cb71b15a10a8b29ee1c17e4ee62daf282
Pedro Vieira <pedrovieira@arrowplan.com>
2021-12-09 11:34:52 +0100

    Add option to delete all checkpoints after a successfull train

--- train.py
+++ train.py
@@ -11,6 +11,7 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+import shutil
 
 from utils.config import VSCNNConfig
 from utils.tools import *
@@ -204,6 +205,9 @@ def train():
     # Save the best model
     best_model_file = cfg.exec_folder + 'best_model.pth'
     torch.save(best_model, best_model_file)
+
+    if cfg.delete_checkpoints:
+        shutil.rmtree(cfg.checkpoint_folder)
 
     if cfg.use_tensorboard:
         writer.close()
