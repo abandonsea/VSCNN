d26636c4c2ab8a6c72389d017bd54e1e2e19348b
Pedro Vieira <pedrovieira@arrowplan.com>
2021-12-20 17:27:40 +0100

    Adapt code from the SAE-3DDRN's noise test to work with the VSCNN models

--- noise_test.py
+++ noise_test.py
@@ -6,21 +6,17 @@ Created on Tue Dec 17 15:33 2021
 @author: Pedro Vieira
 @description: Implements the test function for the SAE-3DDRN network adding noise to the input image
 """
-import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+import numpy as np
 
-from net.drn import DRN
-from net.sae import SAE
+from net.vscnn_paviau import VSCNN
 from test import test_model
-from utils.config import SAE3DConfig
-from utils.dataset import DRNDataset
+from utils.config import VSCNNConfig
+from utils.dataset import VSCNNDataset
 from utils.noise import add_noise
 from utils.tools import *
-
-# Import tensorboard
-from torch.utils.tensorboard import SummaryWriter
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -28,10 +24,9 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 ########################
 # SET TEST CONFIG FILE #
 ########################
-CONFIG_FILE = 'experiments/server_02/config.yaml'  # Empty string to load default 'config.yaml'
-NORMALIZATION = 'minmax'  # minmax; standard
+CONFIG_FILE = 'experiments/server_01/config.yaml'  # Empty string to load default 'config.yaml'
 NOISES = [['salt_and_pepper', 0], ['salt_and_pepper', 0.005], ['salt_and_pepper', 0.01], ['salt_and_pepper', 0.05],
-          ['additive_gaussian', 0.0], ['additive_gaussian', 0.05], ['additive_gaussian', 0.1],
+          ['additive_gaussian', 0.01], ['additive_gaussian', 0.05], ['additive_gaussian', 0.1],
           ['additive_gaussian', 0.2], ['additive_gaussian', 0.3],
           ['multiplicative_gaussian', 0], ['multiplicative_gaussian', 0.1], ['multiplicative_gaussian', 0.3],
           ['multiplicative_gaussian', 0.5], ['multiplicative_gaussian', 0.7]]
@@ -41,48 +36,34 @@ NOISES = [['salt_and_pepper', 0], ['salt_and_pepper', 0.005], ['salt_and_pepper'
 def test():
     # Load config data from training
     config_file = 'config.yaml' if not CONFIG_FILE else CONFIG_FILE
-    cfg = SAE3DConfig(config_file, test=True)
-
-    # Start tensorboard
-    writer = None
-    if cfg.use_tensorboard:
-        writer = SummaryWriter(cfg.tensorboard_folder)
+    cfg = VSCNNConfig(config_file, test=True)
 
     # Set string modifier if testing best models
     test_best = 'best_' if cfg.test_best_models else ''
     if cfg.test_best_models:
         print('Testing best models from each run!')
 
-    # Load processed dataset
-    data = torch.load(cfg.exec_folder + 'proc_image.pth')
+    # Load raw dataset, apply PCA and normalize dataset.
+    data = HSIData(cfg.dataset, cfg.data_folder, cfg.sample_bands)
 
     for run in range(cfg.num_runs):
         print(f'TESTING RUN {run + 1}/{cfg.num_runs}')
 
         _, test_gt, _ = HSIData.load_samples(cfg.split_folder, cfg.train_split, cfg.val_split, run)
-        test_gt = HSIData.remove_negative_gt(test_gt)
-        num_classes = len(np.unique(test_gt)) - 1
-
-        # Load autoencoder model
-        sae_file = cfg.exec_folder + f'runs/sae_model_run_' + str(run) + '.pth'
-        sae = SAE(data.shape[2], cfg.sae_hidden_layers)
-        sae.load_state_dict(torch.load(sae_file, map_location=device))
-        sae.eval()
+        num_classes = len(np.unique(test_gt)) - 1  # Remove one for the "undefined" class
 
         for noise in NOISES:
             print(f'Using {noise[0]} noise with parameter: {noise[1]}')
-            noisy_data = add_noise(data, noise)
-            sae_noisy_data = sae(torch.from_numpy(noisy_data))
-            sae_noisy_data = sae_noisy_data.detach().numpy()
-            sae_noisy_data = HSIData.normalize(sae_noisy_data, normalization=NORMALIZATION)
+            noisy_data = add_noise(data.raw_image, noise)
+            pca_noisy_data = data.apply_transforms(noisy_data)
 
             # Load test ground truth and initialize test loader
-            test_dataset = DRNDataset(sae_noisy_data, test_gt, cfg.sample_size, data_augmentation=False)
+            test_dataset = VSCNNDataset(pca_noisy_data, test_gt, cfg.sample_size, data_augmentation=False)
             test_loader = DataLoader(test_dataset, batch_size=cfg.test_batch_size, shuffle=False)
 
             # Load model
-            model_file = cfg.exec_folder + f'runs/drn_{test_best}model_run_' + str(run) + '.pth'
-            model = nn.DataParallel(DRN(num_classes))
+            model_file = f'{cfg.exec_folder}runs/vscnn_{test_best}model_run_{run}.pth'
+            model = nn.DataParallel(VSCNN(cfg.sample_bands, num_classes))  # Values are going to be overwritten
             model.load_state_dict(torch.load(model_file, map_location=device))
             model.eval()
 
@@ -90,12 +71,9 @@ def test():
             model = model.to(device)
 
             # Test model from the current run
-            report = test_model(model, test_loader, writer)
+            report = test_model(model, test_loader)
             filename = cfg.results_folder + f'test_{noise[0]}.txt'
-            save_results(filename, report, None, run)
-
-    if cfg.use_tensorboard:
-        writer.close()
+            save_results(filename, report, run)
 
 
 # Main for running test independently
